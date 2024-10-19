249e7a0cb66082e519c47404bcb9503521f07e8f
Anton Afanasiev <orderkworinaa@gmail.com>
2024-10-19 16:09:13 +0300

    fix

--- addons/learning_projects/__manifest__.py
+++ addons/learning_projects/__manifest__.py
@@ -19,7 +19,7 @@
         'data/automated_actions/project_stage_id_sync_automated_action.xml',
         'data/automated_actions/project_task_action.xml',
         'data/smtp_maildev_server.xml',
-        # 'views/res_partner_views.xml',
+        'views/res_partner_views.xml',
         'wizards/views/lp_score_master_wizard.xml',
         'wizards/views/lp_score_lecturer_wizard.xml',
         'wizards/views/lp_load_document_wizard.xml',
